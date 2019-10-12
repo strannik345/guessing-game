49c1fa0ff90d9770dbdb66cf512b1a46961b1506
strannik345 <volodkovasya@gmail.com>
2019-10-12 21:40:22 +0300

    feat: done

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,24 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.max = null;
+        this.min =null;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        return Math.round((this.max+this.min)/2);
     }
 
     lower() {
-
+        this.max = Math.round((this.max+this.min)/2);
     }
 
     greater() {
-
+        this.min = Math.round((this.max+this.min)/2);
     }
 }
 
